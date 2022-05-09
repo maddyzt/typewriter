0336a7807c401d7e2b6d31905faf2ea0a84a9931
Maddy Tan <maddytan7@gmail.com>
2022-05-09 21:42:53 +0000

    created typewriter timeout function

---
+++ typewriter.js
@@ -0,0 +1,10 @@
+const sentence = "hello there from lighthouse labs";
+
+let delay = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    console.log(char);
+  }, delay)
+  delay += 50;
+};
